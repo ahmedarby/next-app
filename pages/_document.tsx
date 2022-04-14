0075aed7571fbdb362dcdd3a591e884b8180f912
Ahmed Arby <arby9283@gmail.com>
2022-04-15 00:17:14 +0200

    Final

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render = () => (
-        <Html>
+        <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+                <meta name="theme-color" content="#fff" />
+                <meta name="description" content="Simple app to learn NextJS" />
             </Head>
             <body>
                 <Main />
